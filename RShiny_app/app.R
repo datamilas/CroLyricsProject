1b48489bc681f3bfc81c5e0a375621db834467d0
Ana Milas <amilas@Anas-Air.home>
2022-07-27 00:11:08 +0100

    switch to shinydashboard

--- RShiny_app/app.R
+++ RShiny_app/app.R
@@ -1,13 +1,18 @@
 
 
+
 ## Imports
 library(shiny)
 library(plotly)
 library(dplyr)
 library(shinydashboard)
+library(shinyWidgets)
 
-highlight_color = '#FF9673'
-bars_color = '#E1C8B4'
+highlight_color = "#3d8dbc"
+bars_color = "#d3d3d3"
+
+box_title_style = 'font-size:20px;font-weight: bold'
+
 
 
 ## Data
@@ -24,8 +29,12 @@ adjectives <- read.csv("./Data/adjectives.csv")
 adverbs <- read.csv("./Data/adverbs.csv")
 determiners <- read.csv("./Data/determiners.csv")
 
-singers_song_num = df[!duplicated(df$Song_ID),] %>% count(Artist_name)
+singers_song_num = df[!duplicated(df$Song_ID), ] %>% count(Artist_name)
 all_singers = unique(df$Artist_name)
+num_singers = length(unique(df$Artist_name))
+num_songs = length(unique(df$Song_ID))
+num_words = dim(df)[1]
+
 
 ## Functions
 get_subdf <- function(wordtype) {
@@ -65,8 +74,14 @@ get_subdf_wordsearch <- function(dataframe, words) {
 
 
 get_bar_colors <- function(bars_list, highlighted_bar) {
-  colors = rapply(list(bars_list == highlighted_bar), function(x)
-    ifelse(x, highlight_color, bars_color), how = "replace")
+  if (is.null(highlighted_bar)) {
+    colors = bars_color
+  }
+  else{
+    colors = rapply(list(bars_list == highlighted_bar), function(x)
+      ifelse(x, highlight_color, bars_color), how = "replace")
+  }
+  
   return(unlist(colors))
 }
 
@@ -100,103 +115,213 @@ make_bar_plot <-
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  tags$head(# Note the wrapping of the string in HTML()
-    tags$style(HTML(
-      "
-      body {
-        margin: 50px
-      }"
-    ))),
+ui <- dashboardPage(
+  dashboardHeader(title = "Cro Lyrics Dashboard"),
   
-  
-  titlePanel("Words used in the largest number of songs"),
-  
-  
-  fluidRow(
-    column(
-      2,
-      
-      selectInput(
-        "word_type",
-        "Select POS type",
-        c(
-          "All words",
-          "General Noun",
-          "Proper Noun",
-          "Verb",
-          "Adjective",
-          "Adverb",
-          "Determiner"
-        ),
-        selected = "General Noun"
-      ),
-      radioButtons("topN", "Select number of words to show", c(5, 10, 20), selected = 10),
-      textInput(
-        "search_word",
-        "Or search for word(s)",
-        value = "",
-        width = NULL,
-        placeholder = NULL
-      )
-      
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Explore by words",
+      tabName = "exploreWords",
+      icon = icon("align-left")
     ),
-    
-    
-    
-    column(4,
-           plotlyOutput("barWords")),
-    column(1,),
-    column(
-      1,
-      radioButtons("orderingSingers", "Ordering", c("Descending", "Ascending")),
-      radioButtons(
-        "valueSingers",
-        "Absolute value or percentage?",
-        c("Absolute", "Percentage")
-      )
-    ),
-    column(4,
-           plotlyOutput("barSingers"))
-  ),
-  fluidRow(
-    column(
-      8,
-      textOutput("songsTableTitle"),
-      tags$head(
-        tags$style(
-          "#songsTableTitle{font-size: 20px;
-                                margin-top: 100px; margin-bottom: 15px}"
-        )
-      ),
-      actionButton("singersRestart", "Show all singers"),
-      tags$head(tags$style("#singersRestart{margin-bottom: 15px}")),
-      DT::dataTableOutput("songsTable")
-      
-    ),
-    column(1),
-    column(
-      3,
-      textOutput("zeroSingersTitle"),
-      tags$head(
-        tags$style(
-          "#zeroSingersTitle{font-size: 20px;
-                                margin-top: 100px; margin-bottom: 15px}"
-        )
-      ),
-      tableOutput('zeroSingersTable')
+    menuItem(
+      "Explore by singers",
+      tabName = "exploreSingers",
+      icon = icon("microphone")
     )
+  )),
+  
+  
+  dashboardBody(
+    valueBox(
+      width = 4,
+      tags$p(num_singers, style = "font-size: 120%;"),
+      tags$p("Singers", style = "font-size: 150%;"),
+      icon = icon("microphone"),
+      color = "light-blue"
+    ),
+    valueBox(
+      width = 4,
+      tags$p(num_songs, style = "font-size: 120%;"),
+      tags$p("Songs", style = "font-size: 150%;"),
+      color = "light-blue",
+      icon = icon("music")
+    ),
+    valueBox(
+      width = 4,
+      tags$p(num_words, style = "font-size: 120%;"),
+      tags$p("Words", style = "font-size: 150%;"),
+      color = "light-blue",
+      icon = icon("align-left")
+    ),
+    
+    tabItems(
+      # First tab content
+      tabItem(tabName = "exploreWords",
+              
+              fluidRow(
+                box(
+                  width = 6,
+                  title = p("Words used in the largest number of songs", style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  column(
+                    width = 4,
+                    selectInput(
+                      "word_type",
+                      "Select POS type",
+                      c(
+                        "All words",
+                        "General Noun",
+                        "Proper Noun",
+                        "Verb",
+                        "Adjective",
+                        "Adverb",
+                        "Determiner"
+                      ),
+                      selected = "General Noun"
+                    ),
+                    radioButtons("topN", "Select number of words to show", c(5, 10, 20), selected = 10),
+                    textInput(
+                      "search_word",
+                      "Or search for word(s)",
+                      value = "",
+                      width = NULL,
+                      placeholder = NULL
+                    )
+                  ),
+                  
+                  column(width = 8, plotlyOutput("barWords"))
+                ),
+                
+                
+                box(
+                  width = 6,
+                  title = span(textOutput("barsSingersTitle"), style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  column(
+                    width = 2,
+                    radioButtons("orderingSingers", "Ordering", c("Descending", "Ascending")),
+                    radioButtons(
+                      "valueSingers",
+                      "Absolute value or percentage?",
+                      c("Absolute", "Percentage")
+                    )
+                  ),
+                  
+                  column(width = 8, plotlyOutput("barSingers"))
+                )
+              ),
+              
+              
+              
+              fluidRow(
+                box(
+                  width = 7,
+                  title = span(textOutput("songsTableTitle"), style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  actionButton("singersRestart", "Show all singers"),
+                  tags$head(tags$style("#singersRestart{margin-bottom: 15px}")),
+                  DT::dataTableOutput("songsTable")
+                  
+                ),
+                box(
+                  width = 5
+                  ,
+                  title = span(textOutput("zeroSingersTitle"), style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  tableOutput('zeroSingersTable')
+                  
+                )
+              )),
+      
+      # Second tab content
+      tabItem(tabName = "exploreSingers",
+              fluidRow(
+                box(
+                  width = 7,
+                  title = p("Most used words by singer", style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  
+                  column(
+                    width = 4,
+                    pickerInput(
+                      'singerSelection',
+                      "Select a Singer",
+                      sort(all_singers),
+                      options = list(`live-search` = TRUE)
+                    ),
+                    selectInput(
+                      "word_type_singers",
+                      "Select POS type",
+                      c(
+                        "All words",
+                        "General Noun",
+                        "Proper Noun",
+                        "Verb",
+                        "Adjective",
+                        "Adverb",
+                        "Determiner"
+                      ),
+                      selected = "General Noun"
+                    ),
+                    radioButtons(
+                      "topNSingers",
+                      "Select number of words to show",
+                      c(5, 10, 20),
+                      selected = 10
+                    ),
+                    radioButtons(
+                      "valueWordsSingers",
+                      "Absolute value or percentage?",
+                      c("Absolute", "Percentage")
+                    )
+                  ),
+                  
+                  
+                  column(width = 8, plotlyOutput("wordsBySinger"))
+                  
+                ),
+                
+                box(
+                  width = 5,
+                  title = span(textOutput("songsTableSingersTitle"), style = box_title_style),
+                  solidHeader = FALSE,
+                  status = "primary",
+                  DT::dataTableOutput("songsTableSingers")
+                )
+              ))
+    ),
+    
+    
+    hr(),
+    
+    p(
+      span("Sources:"),
+      a("lyricstranslate.com,", href = "https://lyricstranslate.com/"),
+      a("cuspajz.com,", href = "https://cuspajz.com/"),
+      a("tekstovi.net", href = "https://tekstovi.net/")
+    )
+    
+    
+    
   )
 )
 
 
 
+
+
+
 server <- function(input, output) {
-  
   ## Observe which input changed
   
   # Check if user is using search option or top words
-  top_words_tf <- reactiveVal(TRUE) 
+  top_words_tf <- reactiveVal(TRUE)
   
   observeEvent(input$search_word, {
     top_words_tf(FALSE)
@@ -218,7 +343,8 @@ server <- function(input, output) {
     if (top_words_tf()) {
       temp_df <- get_subdf(input$word_type)
     } else{
-      temp_df <- get_subdf_wordsearch(df_unique_lemmas_all, input$search_word)
+      temp_df <-
+        get_subdf_wordsearch(df_unique_lemmas_all, input$search_word)
     }
     return(temp_df)
   })
@@ -227,7 +353,8 @@ server <- function(input, output) {
   # Get top N words
   top_words_global <- reactive({
     if (top_words_tf()) {
-      top_words <- arrange(temp_df_global() %>% count(lemma), desc(n))[0:input$topN,]
+      top_words <-
+        arrange(temp_df_global() %>% count(lemma), desc(n))[0:input$topN, ]
     } else{
       top_words <- arrange(temp_df_global() %>% count(lemma), desc(n))
     }
@@ -253,11 +380,14 @@ server <- function(input, output) {
     }
     return(highlight_word)
   })
-
+  
   
   # Get dataframe filtered by highlight word
-  temp_df_highlight_word_global <-reactive({
-    return(filter(temp_df_global(), temp_df_global()$lemma == highlight_word_global()))
+  temp_df_highlight_word_global <- reactive({
+    return(filter(
+      temp_df_global(),
+      temp_df_global()$lemma == highlight_word_global()
+    ))
   })
   
   
@@ -274,13 +404,16 @@ server <- function(input, output) {
   
   
   
-  
   ## Outputs
   
   # Top words plot
   output$barWords <- renderPlotly({
     colors = get_bar_colors(top_words_global()$lemma, highlight_word_global())
-    make_bar_plot(top_words_global(), "lemma", "Number of Songs", colors, source =
+    make_bar_plot(top_words_global(),
+                  "lemma",
+                  "Number of Songs",
+                  colors,
+                  source =
                     "barWordsClicks")
   })
   
@@ -318,7 +451,6 @@ server <- function(input, output) {
       categoryorder = "total descending"
     }
     
-    title = sprintf("Singers using the word '%s'", highlight_word_global())
     colors = get_bar_colors(top_words_singers$Artist_name, clicked_singer())
     
     make_bar_plot(
@@ -328,39 +460,44 @@ server <- function(input, output) {
       colors,
       source = "barSingersClicks",
       categoryorder = categoryorder,
-      title = title
     )
+    
   })
   
+  
+  # Render title of barSingers plot
+  output$barsSingersTitle <- renderText({
+    sprintf("Singers using the word '%s'", highlight_word_global())
+    
+  })
   
   
   # Table of songs
   output$songsTable = DT::renderDataTable({
-
     temp_df_table <- temp_df_highlight_word_global()
     if (clicked_singer() != "") {
-      temp_df_table <- filter(temp_df_table, temp_df_table$Artist_name == clicked_singer())
+      temp_df_table <-
+        filter(temp_df_table,
+               temp_df_table$Artist_name == clicked_singer())
     }
-
-
+    
     temp_df_table$Lyrics_url <-
-      paste0(
-        "<a href='",
-        temp_df_table$Lyrics_url,
-        "'>",
-        temp_df_table$Lyrics_url,
-        "</a>"
-      )
+      paste0("<a href='",
+             temp_df_table$Lyrics_url,
+             "'>",
+             temp_df_table$Song,
+             "</a>")
     
     temp_df_table <-
-      temp_df_table[c("Artist_name", "Song", "Lyrics_url", "Views")]
+      temp_df_table[c("Artist_name", "Lyrics_url", "Views")]
     colnames(temp_df_table) <-
-      c("Singer", "Song", "Lyrics", "Views")
+      c("Singer", "Song", "Views")
     
     temp_df_table <-
-      temp_df_table[order(temp_df_table$Views, decreasing = TRUE), ]
+      temp_df_table[order(temp_df_table$Views, decreasing = TRUE),]
     
-    rownames(temp_df_table) <- NULL        # Resetting index numbers of rows
+    rownames(temp_df_table) <-
+      NULL        # Resetting index numbers of rows
     temp_df_table
     
   }, escape = FALSE)
@@ -368,18 +505,19 @@ server <- function(input, output) {
   
   # Render title of table of songs
   output$songsTableTitle <- renderText({
-
     if (clicked_singer() == "") {
       sprintf("Songs mentioning the word '%s'", highlight_word_global())
     }
     else{
-      sprintf("Songs mentioning the word '%s' by %s",
-              highlight_word_global(),
-              clicked_singer())
+      sprintf(
+        "Songs mentioning the word '%s' by %s",
+        highlight_word_global(),
+        clicked_singer()
+      )
     }
   })
   
-  
+  # Render the table of singers not using the word
   output$zeroSingersTable <- renderTable({
     count_songs = temp_df_highlight_word_global() %>% count(Artist_name)
     
@@ -387,13 +525,144 @@ server <- function(input, output) {
       all_singers[!(all_singers %in% count_songs$Artist_name)]   #get singers that do not mention the word
     zero_singers_df <- data.frame(unlist(zero_singers))
     colnames(zero_singers_df) <- c("Singer")
-    zero_singers_df
-  })
-  
-  output$zeroSingersTitle <- renderText({
     
+    if (dim(zero_singers_df)[1] > 0) {
+      zero_singers_df
+    }
+    else{
+      sprintf(
+        "All singers in the databse use the word '%s' in at least one of their songs",
+        highlight_word_global()
+      )
+    }
+    
+  }, colnames = FALSE)
+  
+  # Render the title of singers that are not using the word
+  output$zeroSingersTitle <- renderText({
     sprintf("Singers not using the word '%s'", highlight_word_global())
   })
+  
+  
+  
+  ### Explore by singers dashboard
+  
+  # Follow which word is clicked
+  
+  temp_df_singers_global <- reactive({
+    temp_df <- get_subdf(input$word_type_singers)
+    return(temp_df)
+  })
+  
+  top_words_singers_global <- reactive({
+    temp_df <- temp_df_singers_global() %>%
+      filter(Artist_name == input$singerSelection) %>%
+      count(lemma) %>%
+      arrange(desc(n))
+    
+    if (dim(temp_df)[1] >= strtoi(input$topNSingers)) {
+      temp_df <-
+        temp_df %>% filter(temp_df$n >= temp_df[input$topNSingers, "n"])
+    }
+    
+    return(temp_df)
+  })
+  
+  
+  
+  clicked_word_singers <- reactiveVal(NULL)
+  observeEvent(event_data("plotly_click", source = "wordsBySingerClicks"),
+               {
+                 clicked_word_singers(event_data("plotly_click", source = "wordsBySingerClicks")$y)
+               })
+  
+  
+  observeEvent(list(input$word_type_singers, input$singerSelection), {
+    clicked_word_singers(NULL)
+  })
+  
+  # If no word is clicked by the user highlight top word
+  highlight_word_singers_global <- reactive({
+    if (is.null(clicked_word_singers())) {
+      highlight_word <- top_words_singers_global()[1, "lemma"]
+    }
+    else{
+      highlight_word <- clicked_word_singers()
+    }
+    return(highlight_word)
+  })
+  
+  ## Outputs
+  
+  
+  # Words used by the singer
+  output$wordsBySinger <- renderPlotly({
+    # Get dataframe filtered by word type
+    
+    x_label <- "Number of Songs"
+    top_words <- top_words_singers_global()
+    
+    if (input$valueWordsSingers == "Percentage") {
+      total_sn <-
+        filter(singers_song_num,
+               singers_song_num$Artist_name == input$singerSelection)$n
+      print(total_sn)
+      top_words$n <- top_words$n / total_sn * 100
+      x_label <- "Percentage of songs"
+    }
+    
+    colors = get_bar_colors(top_words$lemma, highlight_word_singers_global())
+    
+    make_bar_plot(
+      top_words,
+      "lemma",
+      colors = colors,
+      x_label = x_label,
+      source = "wordsBySingerClicks"
+    )
+  })
+  
+  
+  
+  # Table of songs
+  output$songsTableSingers = DT::renderDataTable({
+    temp_df_table <- temp_df_singers_global() %>%
+      filter(Artist_name == input$singerSelection &
+               lemma == highlight_word_singers_global()) %>%
+      distinct(Song_ID, .keep_all = TRUE) %>%
+      arrange(desc(Views))
+    
+    
+    temp_df_table$Lyrics_url <-
+      paste0("<a href='",
+             temp_df_table$Lyrics_url,
+             "'>",
+             temp_df_table$Song,
+             "</a>")
+    
+    temp_df_table <-
+      temp_df_table[c("Lyrics_url", "Views")]
+    colnames(temp_df_table) <-
+      c("Song", "Views")
+    
+    rownames(temp_df_table) <-
+      NULL        # Resetting index numbers of rows
+    temp_df_table
+    
+  }, escape = FALSE)
+  
+  
+  # Render title of table of songs by singer
+  output$songsTableSingersTitle <- renderText({
+    sprintf(
+      "Songs by %s mentioning the word '%s'",
+      input$singerSelection,
+      highlight_word_singers_global()
+    )
+    
+  })
+  
+  
   
   
 }
